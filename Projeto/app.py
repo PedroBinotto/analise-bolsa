07e00ea78d08b04c6cb87af917eda9cce6f5cbe4
Pedro Binotto <pedrosantibinotto@gmail.com>
2020-05-25 21:50:23 -0300

    YFinance + Flask-WTF

--- Projeto/app.py
+++ Projeto/app.py
@@ -1,59 +1,33 @@
-import os
-import datetime
-import time
-from iexfinance.stocks import Stock
-from iexfinance.refdata import get_symbols
+import yfinance as yf
 from flask import Flask, request, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, PasswordField
+from wtforms.validators import Length, InputRequired
+import datetime
+import json
+import decimal
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-os.environ['IEX_TOKEN'] = 'pk_19313062487e43ceaedce40e63973a12'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/pedro/Desktop/Programas/investimento-joao/Projeto/stocks.db'
-app.config['SECRET_KEY'] = 'mysecret'
-db = SQLAlchemy(app)
 
-class Watch(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	symbol = db.Column(db.String(4), unique=True)
-	name = db.Column(db.String(20))
+msft = yf.Ticker("MSFT")
+print(msft.info['regularMarketPrice'])
 
-class Hist(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	symbol = db.Column(db.String(4), db.ForeignKey('watch.symbol'))
-	price = db.Column(db.Float)
-	datetime = db.Column(db.DateTime)
-
-db.create_all()
-
-# Log
-def logInfo():
-	while True:
-		try:
-			symbol = 'AAPL'
-			a = Stock(symbol, token=os.environ['IEX_TOKEN'])
-			print( str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),\
-			 ' PRICE: ' + str(a.get_quote()['latestPrice']))
-			time.sleep(60.0)
-		except:
-			print('\nErro!')
-			break
-
-# logInfo()
-# Read
-
-'''
-for i in get_symbols():
-	print(i['symbol'], i['name'])
-'''
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	symbols = []
-	for i in get_symbols():
-		tmp = [i['symbol'], i['name']]
-		symbols.append(tmp)
-	return render_template('index.html', s=symbols)
+	if request.method == 'GET':
+		return render_template('index.html')
+	else:
+		return redirect('/result')	
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+	return render_template('result.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
