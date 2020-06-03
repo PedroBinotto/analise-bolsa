bee926e3cb8d1638fc8e6061c1c6833137da91c7
Pedro Binotto <pedrosantibinotto@gmail.com>
2020-06-03 00:51:52 -0300

    Donwload de PDF

--- Aplicativo/Projeto/static/JS/app.js
+++ Aplicativo/Projeto/static/JS/app.js
@@ -50,46 +50,74 @@ function createControll(NoOfRec) {
 }
 
 function jsonData(json) {
-  cab = ['Ação', 'Valor total'];
-  json_array.push(cab);
+    cab = ['Ação', 'Valor total'];
+    json_array.push(cab);
 
-  for (var j = 0; j < json.length; j++) { // Elementos [1] e [4]
+    for (var j = 0; j < json.length; j++) { // Elementos [1] e [4]
     var array2 = [0, 3];
     json[j] = array2.map(i => json[j][i]);
 
     json_array.push(json[j]);
-  }
+    }
 }
-
+    
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
-  if (json_array.length < 2) {
-    var apology = document.getElementById('chart_div');
-    apology.innerHTML += '';
-    return;
-  }
+    if (json_array.length < 2) {
+        var apology = document.getElementById('chart_div');
+        apology.innerHTML += '';
+        return;
+    }
 
-  var data = google.visualization.arrayToDataTable(json_array);
+    var data = google.visualization.arrayToDataTable(json_array);
 
-  var options = {
+    var options = {
     title: 'Análise Superficial',
     pieHole: 0.4,
     chartArea: {
-      height: '94%',
-      width: '100%'
+        height: '94%',
+        width: '100%'
     },
     width: '100%',
     height: 800,
     legend: {
-      alignment: 'center',
-      position: 'bottom'
+        alignment: 'center',
+        position: 'bottom'
     }
-  };
+    };
 
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    var btnSave = document.getElementById('save-pdf');
+
+    google.visualization.events.addListener(chart, 'ready', function () {
+        btnSave.disabled = false;
+    });
+
+    btnSave.addEventListener('click', function () {
+        var hght = $('#print').height();
+        console.log(hght);
+        var wdth = $(window).width();
+        console.log(wdth);
+        var doc = new jsPDF({
+            unit: 'px',
+            format: [hght, wdth]
+        });
+        canvas = chart.getImageURI();
+        doc.addImage(canvas, 0, 0,);
+        doc.save('chart.pdf');
+    }, false);
+
+    chart.draw(data, options);
 }
+
+/*
+{
+    orientation: 'landscape',
+    unit: 'in',
+    format: [4, 2]
+}
+*/
